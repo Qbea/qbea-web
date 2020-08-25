d361a55e1b6d5a661c5c27163eea25856dd0c9f0
yujl <Yjl1317945369>
2020-08-25 18:19:36 +0800

    登录页、主页更新，去除eslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: ()=> import('../views/home/Login.vue')
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router
